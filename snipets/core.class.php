1330544f139f8b453a4f85b5c1742425e4acdfd0
elex <elextraza@gmail.com>
2015-10-01 15:09:54 +0300

    Initial commit

---
+++ snipets/core.class.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: elex
+ * Date: 01.10.15
+ * Time: 15:05
+ */
+
+class Spinky
+{
+
+    //Список пунктов меню
+    function GetMenuList()
+    {
+        global $modx;
+        global $table_prefix;
+
+    }
+
+    //список товаров в корзине
+    function GetCardList()
+    {
+
+    }
+}
