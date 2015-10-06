985642c7add74566567161f2ea69be476b5a9a88
elex <elextraza@gmail.com>
2015-10-06 14:13:59 +0300

    вывод левого меню, страницы

--- js/core.js
+++ js/core.js
@@ -33,12 +33,53 @@ function GetMainPage()
 function GetCard()
 {
     ShowPage("/index/card.html",'card');
-
-
 }
 
 
-$(function() {
+
+    //добавление в корзину
+    function AddToCard(product_id, product_count, product_price) {
+        console.info("AddToCard=" + product_id);
+        $.get(
+            "ajax.html",
+            {
+                //log1:1,
+                action: "AddToCard",
+                product_id: product_id,
+                product_count: product_count,
+                product_price: product_price
+            },
+            function (data) {
+                console.info(data);
+                //меняла чекнуто не екнуто
+                if (data.status == "1") {
+                    $(".product_content_buy").css("cursor", "no-drop").attr("disabled", "disabled");
+                }
+                else {
+                    $(".product_" + product_id).css("background-position", "");
+                }
+
+                if (parseInt(data.count) > 0) {
+                    var tmp = "<a href='/cart/'><img src='/assets/bonsan/tpl/img/cart.png' alt='Корзина заказов'/><p><span>В вашей корзине:</span>";
+                    tmp = tmp + "<br>" + String(data.count) + " товаров на сумму";
+                    tmp = tmp + "<br>" + String(data.summa) + " руб.</p></a>";
+                    $(".top2_cart").html(tmp);
+                   // $('.modal').modal('show');
+                    /*setTimeout(function () {
+                     $('.modalThx').modal('hide');
+                     }, 5000);*/
+                }
+                /*else {
+                 $(".top2_cart").html('')
+                 }*/
+
+            }, "json"
+        ); //$.get  END
+
+    }
+
+
+    $(function() {
     GetConstructor();
     GetCard();
     GetMainPage();
--- snipets/core.class.php
+++ snipets/core.class.php
@@ -133,13 +133,141 @@ class Spinky
     //список товаров в корзине
     function GetCardList()
     {
-
+        $menuItems=$this->GetMenuList();
+        foreach($menuItems as $menuItem)
+        {
+            ?>
+            <h3 class="submenuitem click"  onclick="ShowPage('<?php echo $menuItem->url; ?>','CenterBox');"><?php echo $menuItem->title;?></h3>
+        <?php
+        }
     }
 
-    function GetProductsListFromCategory($category_id)
+    function ShowMainMenu()
+    {
+        $menuItems=$this->GetMenuList();
+        foreach($menuItems as $menuItem)
+        {
+            ?>
+            <div class="centersep">
+                <h4 class="centermenusubtitle"><?php echo $menuItem->title; ?></h4>
+            </div>
+            <?php
+            $category=$this->GetCategoryProducts($menuItem->id);
+            foreach($category as $product)
+            {
+                ?>
+                <div class="centeritem"><img class="centeritemimg" src="/product-images/<?php echo $product->tv['img'];?>">
+                    <h5 class="centeritemtitle"><?php echo $product->title;?></h5>
+                    <div class="price centeritemprice"><?php echo $product->tv['Price'];?>&nbsp;₽</div>
+                    <div class="dobavit click" onclick="AddToCard(<?php echo $product->id;?>,1)">Добавить в заказ</div>
+                </div>
+
+                <?php
+            }
+        }
+    }
+
+    /*Показывает товары в одной категории*/
+    function ShowCategorySingle($category_id)
+    {
+        $category=$this->GetCategoryProducts($category_id);
+        foreach($category as $product)
+        {
+            ?>
+            <div class="centeritem">
+                <img class="centeritemimg" src="/product-images/<?php echo $product->tv['img'];?>">
+                <h5 class="centeritemtitle"><?php echo $product->title;?></h5>
+                <div class="price centeritemprice"><?php echo $product->tv['Price'];?>&nbsp;₽</div>
+                <div class="dobavit click"  onclick="AddToCard(<?php echo $product->id;?>,1,<?php echo $product->tv['Price'];?>)">Добавить в заказ</div>
+            </div>
+
+        <?php
+        }
+    }
+
+    //влзвращает массив страниц категории
+    function GetCategoryProducts($category_id)
     {
         global $modx;
         global $table_prefix;
+        $parent=$category_id;
+        $sql = "select * from " . $table_prefix . "site_content where parent=".$parent  ;
+        $tmp = Array();
+        foreach ($modx->query($sql) as $row)
+        {
+            $tmp[]=GetPageInfo($row['id']);
+        }
+        return $tmp;
+    }
+
+
+
+//Возвращает количество товаров в корзине
+    function GetCardCountProduct()
+    {
+        $cc = 0;
+        foreach ($_SESSION as $key => $value) {
+            if (substr($key, 0, 3) == 'pro') {
+                $cc = $cc + $value;
+            };
+            //echo $key." ".$value." ".substr($key,0,3);
+
+
+        }
+        return $cc;
+    }
+
+    function GetTotalPrice()
+    {
+        $price = 0;
+        $cc=0;
+        foreach ($_SESSION as $key => $value) {
+            if (substr($key, 0, 3) == 'pro') $cc = $cc + $value;
+            $dd = explode(' ', $value);
+            $price += $dd[0] * $dd[1];
+        }
+        return $price;
+    }
+
+
+    //Для обработка ajax-запросов
+    function Ajax()
+    {
+        if(isset($_GET['action']))
+        {
+            if($_GET['action']=='AddToCard')
+            {
+                AddToCard();
+            }
+        }
+    }
+
+
+    function AddToCard()
+    {
+
+        $product_price = mysql_escape_string($_GET['product_price']);
+        $product_count = mysql_escape_string($_GET['product_count']);
+        if(!isset($_SESSION['product_'.$_GET['product_id']])){
+            $_SESSION['product_' . $_GET['product_id']] = $product_count.' '.$product_price;
+        } else {
+            $dd = explode(" ",$_SESSION['product_' . $_GET['product_id']]);
+            $dd[0] = $dd[0] + $product_count;
+            $_SESSION['product_' . $_GET['product_id']] = $dd[0]." ".$dd[1];
+        }
+
+
+        //посчитать сумму и рублей
+        //вычисляем сумму
+        //----------------------------------------------------------
+        //----------------------------------------------------------
+        $summa = 0;
+
+        $summa = GetTotalPrice();
+        //----------------------------------------------------------
+        //----------------------------------------------------------
+        echo json_encode(array("status" => "1", "count" => GetCardCountProduct(), "summa" => $summa)); //добавили
+        //  }
     }
 
     function Run($scriptProperties)
@@ -149,7 +277,11 @@ class Spinky
             if($scriptProperties['action']=='ShowLeftMenu')
             {
                 $this->ShowLeftMenu();
-            }
+            }//ShowMainMenu
+            elseif($scriptProperties['action']=='ShowMainMenu')
+            {
+                $this->ShowMainMenu();
+            }//
         }
     }
 }
